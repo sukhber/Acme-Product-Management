827a165ddcc1bd820d22286993afa4324faf4fb8
sukhber.bansal <sukhber.bansal@iongroup.com>
2019-08-14 15:25:26 +0530

    Intoduction to modules and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
+})
+
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
+
+
+
+/*import { Component } from '@angular/core';
+
+@Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>Hello Baby</div>`
 })
 export class AppComponent {
   title = 'employee-management';
 }
+//templateUrl: './app.component.html',
+//styleUrls: ['./app.component.css']
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ProductListComponent } from './products/product-list.component'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
