c26e6e5fa22d15b4c4bf962d47c4ffc77109f196
sukhber <sukhberbansal@gmail.com>
2019-08-22 19:25:22 +0530

    view child

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
@@ -8,18 +8,20 @@ import { ProductService } from './product.service';
 }
 
 )
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, AfterViewInit{
     pageTitle: string = 'Product List ';
     imageWidth: number = 60;
     imageMargin: number = 5;
-    _listFilter: string;
+    private _listFilter: string;
     showImage: boolean = true;
     buttonText: string = 'Hide Image';
     filterOption: string;
     ratingMessage: string;
     errorMessage: string;
     products: IProduct[];
-    _filteredProducts: IProduct[];
+
+    @ViewChild('filterElement', {static: false}) filterElementReference: ElementRef;
+
       toggleImage(): void {
           this.showImage = !this.showImage;
           this.buttonText = this.showImage ? 'Hide Image' : 'Show Image';
@@ -35,7 +37,7 @@ export class ProductListComponent implements OnInit{
           } );
       };
       constructor(private productService: ProductService) {
-        this.filterOption = 'all';
+        this.filterOption = 'name';
         this.listFilter = '';
       };
       get listFilter (): string {
@@ -45,27 +47,25 @@ export class ProductListComponent implements OnInit{
           this._listFilter = value;
       };
 
-      get filteredProducts (): IProduct[] {
-
+      get filteredProducts(): IProduct[] {
         if (this.filterOption === 'all') {
-        this._filteredProducts = this.products;
-       } else if (this.filterOption === 'name') {
-        this._filteredProducts = this.products.filter( product => product.productName.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
-       } else if (this.filterOption === 'code') {
-        this._filteredProducts = this.products.filter( product => product.productCode.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
-       } else if (this.filterOption === 'availableDate') {
-        this._filteredProducts = this.products.filter( product => product.releaseDate.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
-       } else if (this.filterOption === 'price') {
-        this._filteredProducts = this.products.filter( product => product.price.toLocaleString().indexOf( this.listFilter ) !== -1 );
-       } else if (this.filterOption === 'rating') {
-        this._filteredProducts = this.products.filter( product => product.starRating.toLocaleString().indexOf( this.listFilter ) !== -1 );
-       }
-       return this._filteredProducts;
-      };
-      set filteredProducts (value: IProduct[]) {
-          this._filteredProducts = value;
-      }
+            return this.products;
+        } else if (this.filterOption === 'name') {
+            return this.products.filter( product => product.productName.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
+        } else if (this.filterOption === 'code') {
+            return this.products.filter( product => product.productCode.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
+        } else if (this.filterOption === 'availableDate') {
+            return this.products.filter( product => product.releaseDate.toLocaleLowerCase().indexOf( this.listFilter.toLocaleLowerCase() ) !== -1 );
+        } else if (this.filterOption === 'price') {
+            return this.products.filter( product => product.price.toLocaleString().indexOf( this.listFilter ) !== -1 );
+        } else if (this.filterOption === 'rating') {
+            return this.products.filter( product => product.starRating.toLocaleString().indexOf( this.listFilter ) !== -1 );
+        }
+    };
     onRatingClicked(message: string): void {
         this.ratingMessage = message;
+    };
+    ngAfterViewInit(): void {
+        this.filterElementReference.nativeElement.focus();
     };
 }
