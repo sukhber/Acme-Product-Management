b2d3f8f443c879f870d587f06b40f59719474bad
sukhber <sukhberbansal@gmail.com>
2019-08-26 01:53:03 +0530

    product detail guard minor changes

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { ProductService } from './product.service';
 
 @Injectable()
 
 export class ProductDetailGuard implements CanActivate {
 
-  productIds: number[];
-
-  constructor(private router: Router, private productService: ProductService) {
-
-  }
+  constructor(private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       let id = +next.url[1].path;
-      this.productService.getProducts().subscribe({
-        next: products => this.productIds = products.map(product => product.id)
-      });
-      if( isNaN(id) || !this.productIds.includes(id) ) {
+      if( isNaN(id)) {
         alert('Invalid Product Id');
         this.router.navigate(['/products']);
         return false;
